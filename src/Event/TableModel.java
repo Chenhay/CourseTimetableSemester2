00daa4892d3e14a2f014b7ed9cac3e8067f26680
TryChenhai <chenhaitry64@gmail.com>
2019-04-22 20:32:43 +0700

    Just Review

--- src/Event/TableModel.java
+++ src/Event/TableModel.java
@@ -8,30 +8,26 @@ import javax.swing.table.DefaultTableModel;
 
 public class TableModel {
     
-    public static DefaultTableModel buildTableModel(ResultSet rs)
-        throws SQLException {
+    public static DefaultTableModel buildTableModel(ResultSet rs)throws SQLException {
+        ResultSetMetaData metaData = rs.getMetaData();
 
-    ResultSetMetaData metaData = rs.getMetaData();
-
-    int columnCount = metaData.getColumnCount();
-    // names of columns
-    Vector<String> columnNames = new Vector<String>();
-    for (int column = 1; column <= columnCount; column++) {
-        columnNames.add(metaData.getColumnName(column));
-    }
-
-    // data of the table
-    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
-    while (rs.next()) {
-        Vector<Object> vector = new Vector<Object>();
-        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-            vector.add(rs.getObject(columnIndex));
+        int columnCount = metaData.getColumnCount();
+        // names of columns
+        Vector<String> columnNames = new Vector<String>();
+        for (int column = 1; column <= columnCount; column++) {
+            columnNames.add(metaData.getColumnName(column));
         }
-        data.add(vector);
-    }
 
+        // data of the table
+        Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+        while (rs.next()) {
+            Vector<Object> vector = new Vector<Object>();
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                vector.add(rs.getObject(columnIndex));
+            }
+            data.add(vector);
+        }
     return new DefaultTableModel(data,columnNames);
 
-}
-    
+    } 
 }
